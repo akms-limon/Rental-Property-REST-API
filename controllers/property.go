5d7e11c51105e5e242afbd42089090f90d94ad81
A K M S Limon office <akmslimon@gmail.com>
2026-09-22 15:28:14 +0600

    Feat: API Structure and routing skleton

--- controllers/property.go
+++ controllers/property.go
@@ -1,5 +1,24 @@
 package controllers
 
-import "Rental-Property-REST-API/services"
+import (
+	"Rental-Property-REST-API/services"
 
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+// Keeping a global variable for PropertyService to be used in the controller methods
+// and the assignment operation is done in main.go
 var PropertyService  *services.PropertyService
+
+
+type PropertyController struct {
+	beego.Controller  // Beego gives us request/response information through the embedded controller
+}
+
+func (controller *PropertyController) GetProperties() {
+	controller.Ctx.WriteString("GetProperties called");
+}
+
+func (controller *PropertyController) GetProperty() {
+	controller.Ctx.WriteString("GetProperty called");
+}
--- routers/router.go
+++ routers/router.go
@@ -8,9 +8,20 @@
 package routers
 
 import (
+	"Rental-Property-REST-API/controllers"
 
+	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
-	
+
+	// Route for our API endpoints
+	ns := beego.NewNamespace("/v1",
+		beego.NSNamespace("/properties",
+			beego.NSRouter("", &controllers.PropertyController{}, "get:GetProperties"),
+			beego.NSRouter("/:id", &controllers.PropertyController{}, "get:GetProperty"),
+		),
+	)
+
+	beego.AddNamespace(ns)
 }
